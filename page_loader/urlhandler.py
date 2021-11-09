f59c833714839e4b102dcc20455df8d5242f3b12
justpwned <justpwnedfrom@gmail.com>
2021-11-09 16:14:03 +0500

    urlhandler refactor

--- page_loader/urlhandler.py
+++ page_loader/urlhandler.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse, ParseResult, urljoin
+import urllib.parse
 import os.path
 import re
 import mimetypes
@@ -6,67 +6,69 @@ import mimetypes
 mimetypes.init()
 
 
-def get_mime_ext(content_type):
-    return mimetypes.guess_extension(content_type, strict=False) or ''
+def _get_mime_ext(content_type):
+    return mimetypes.guess_extension(content_type, strict=False)
 
 
-def transform_url(url):
+def _url_to_path(url):
     return re.sub(r'[^0-9a-zA-Z]', '-', url)
 
 
 class UrlHandler:
     def __init__(self, url):
-        urlparsed = urlparse(url)
-        self.scheme = urlparsed.scheme
-        self.netloc = urlparsed.netloc
-        path, etc = urlparsed.path, urlparsed[3:]  # iterable
-        if path == '/':
-            path = ''
-        self.path = (path,) + etc
+        self._url = urllib.parse.urlsplit(url)
+
+    @property
+    def scheme(self):
+        return self._url.scheme
+
+    @property
+    def netloc(self):
+        return self._url.netloc
+
+    @property
+    def path(self):
+        return self._url.path
+
+    @property
+    def query(self):
+        return self._url.query
+
+    @property
+    def fragment(self):
+        return self._url.fragment
 
     @property
     def is_local(self):
         return len(self.scheme) == 0 and len(self.netloc) == 0
 
     def get_url(self, strip_scheme=False):
-        scheme = self.scheme if not strip_scheme else ''
-        url = ParseResult(scheme, self.netloc, *self.path).geturl()
-        if strip_scheme:
-            return url.replace(r'//', '', 1)
-        return url
+        if not strip_scheme:
+            return self._url.geturl()
 
-    def to_filepath(self, out_dir='', mimetype='', *, only_netloc=False):
-        """
-        Note: If extension is not present in the "path" part of the url,
-        function uses supplied mimetype to guess the extension. If successful
-        it appends according extension to the filepath, otherwise it leaves
-        url as is.
-        """
-        if only_netloc:
-            return os.path.join(out_dir, transform_url(self.netloc))
-
-        url = self.get_url(strip_scheme=True)
-
-        gussed_ext = get_mime_ext(mimetype)
-        if not self.path[0]:
-            fullname = transform_url(url) + gussed_ext
-        else:
-            new_url, ext = os.path.splitext(url)
-            if ext == '':
-                ext = gussed_ext
-            fullname = transform_url(new_url) + ext
-
-        return os.path.join(out_dir, fullname)
+        scheme = f'{self.scheme}://'
+        return self._url.geturl().replace(scheme, '', 1)
 
     def join(self, other_url):
         if isinstance(other_url, UrlHandler):
             other_url = other_url.get_url()
-        return urljoin(self.get_url(), other_url)
+        joined_url = urllib.parse.urljoin(self.get_url(), other_url)
+        return UrlHandler(joined_url)
 
-    @staticmethod
-    def urljoin(url1, url2):
-        if isinstance(url1, UrlHandler):
-            url1 = url1.get_url()
-        if isinstance(url2, UrlHandler):
-            url2 = url2.get_url()
-        return urljoin(url1, url2)
+    def to_filepath(self, include_ext=True, mimetype=''):
+        """
+        How to determine file extension?
+        1) if path has file extension then use it
+        2) if mimetype has been provided then use it if filepath has no extension,
+           otherwise - ignore mimetype and use path extension
+        3) if neither filepath extension nor mimetype is present, use '.html'
+        """
+        if not include_ext:
+            return _url_to_path(self.get_url(strip_scheme=True))
+
+        path, ext = os.path.splitext(self.path)
+        if not ext:
+            guessed_ext = _get_mime_ext(mimetype)
+            ext = guessed_ext if guessed_ext is not None else '.html'
+
+        return _url_to_path(self.netloc + path) + ext
--- tests/test_urlhandler.py
+++ tests/test_urlhandler.py
@@ -1,36 +1,25 @@
 from page_loader.urlhandler import UrlHandler
 
-test_url = 'https://example.com/hello/there'
+TEST_URL = 'https://example.com/hello/there'
 
 
 def test_geturl():
-    handler = UrlHandler(test_url)
+    handler = UrlHandler(TEST_URL)
     no_scheme_test_url = 'example.com/hello/there'
-    assert handler.get_url() == test_url
+    assert handler.get_url() == TEST_URL
     assert handler.get_url(strip_scheme=True) == no_scheme_test_url
 
 
 def test_local():
+    handler = UrlHandler(TEST_URL)
     local_url = '/hello/there'
-    handler = UrlHandler(test_url)
     local_handler = UrlHandler(local_url)
     assert handler.is_local is False
     assert local_handler.is_local is True
 
 
-def test_to_filepath():
-    handler = UrlHandler(test_url)
-    assert handler.to_filepath(only_netloc=True) == 'example-com'
-    assert handler.to_filepath() == 'example-com-hello-there'
-
-
-def test_to_filepath_ext():
-    ext_handler = UrlHandler('https://example.com/hello/there.jpeg')
-    domen_handler = UrlHandler('https://example.com')
-    test_url_handler = UrlHandler(test_url)
-    assert ext_handler.to_filepath() == 'example-com-hello-there.jpeg'
-    assert ext_handler.to_filepath('assets') == 'assets/example-com-hello-there.jpeg'
-    assert test_url_handler.to_filepath(mimetype='text/html') == 'example-com-hello-there.html'
-
-    assert domen_handler.to_filepath() == 'example-com'
-    assert domen_handler.to_filepath(mimetype='text/html') == 'example-com.html'
+def test_to_filepath_():
+    handler = UrlHandler(TEST_URL)
+    assert handler.to_filepath() == 'example-com-hello-there.html'
+    assert handler.to_filepath(mimetype='image/jpeg') == 'example-com-hello-there.jpg'
+    assert handler.to_filepath(include_ext=False, mimetype='image/jpeg') == 'example-com-hello-there'
